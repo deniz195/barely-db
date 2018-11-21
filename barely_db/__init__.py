a0b9617684ffbf0922d9f1cbef2c0321588894c6
Deniz Bozyigit <dbozyigit@battrion.com>
2018-11-21 16:18:52 +0100

    first working version

--- barely_db/__init__.py
+++ barely_db/__init__.py
@@ -15,9 +15,6 @@ import objectpath # http://objectpath.org/reference.html
 module_logger = logging.getLogger(__name__)
 module_logger.setLevel(logging.DEBUG)
 
-
-from .db import *
-
 # general useful module components
 def _reload_module():
     import sys
@@ -174,11 +171,68 @@ def test_BUID():
 
 # BUID.buid_regex = re.compile(BUID.buid_regex_str)
 
+# class SourcedItem(float):
+#     name = None
+#     value = None
+#     property_file = None
+#     source = None
+
+#     @classmethod
+#     def make(cls, name, value, property_file, source):       
+#         new_item = cls(value)
+#         new_item.name = name
+#         new_item.property_file = property_file
+#         new_item.source = source
+
+#     def __init__(self, name, value, property_file, source):
+#         self.name = name
+#         self.value = value
+#         self.property_file = property_file
+#         self.source = source
+
+#     def as_dict(self):
+#         return {self.name: {'value': self.value, 'property_file': self.property_file, 'source': self.source}}
+
+#     def __str__(self):
+#         return f'{self.name} = {self.value} (from {self.property_file}, {self.source})'
+
+#     def __repr__(self):
+#         return "%s(**%s)" % (self.__class__.__qualname__, self.__dict__)
+
+
+# class SourcedItem(float):
+#     name = None
+#     value = None
+#     property_file = None
+#     source = None
+
+#     @classmethod
+#     def make(cls, name, value, property_file, source):       
+#         new_item = cls(value)
+#         new_item.name = name
+#         new_item.property_file = property_file
+#         new_item.source = source
+#         return new_item
+
+#     def as_dict(self):
+#         return {self.name: {'value': self.value, 'property_file': self.property_file, 'source': self.source}}
+
+#     def __str__(self):
+#         return f'{self.name} = {self.value} (from {self.property_file}, {self.source})'
+
+#     def __repr__(self):
+#         return "%s(**%s)" % (self.__class__.__qualname__, self.__dict__)
+
+
+from collections import namedtuple
+SourcedItem = namedtuple('SourcedItem', 'name, value, property_file, source')
+
+
 class BarelyDB(object):
 
     base_path = None
     property_file_glob = '*.property.json'
-
+    preferred_property_files = []
 
     def __init__(self, base_path):
         self.base_path = Path(base_path)
@@ -213,16 +267,25 @@ class BarelyDB(object):
             path = path.resolve().absolute()
         return path
 
-    def entity_files(self, buid, glob):
+    def entity_files(self, buid, glob, must_contain_buid = False, output_as_str=True):
         buid = self.buid_normalizer(buid)        
         path = self.entity_path(buid)
         files = path.glob(glob)
+
+        if must_contain_buid:
+            buid_p = BUIDParser(ignore_unknown=False, mode = 'first', warn_empty = False)
+            files_sel = [fn for fn in files if (buid_p(fn) == buid)]
+            files = files_sel
+
+        if output_as_str:
+            files = [str(fn) for fn in files]
+
         return list(files)
 
 
-    def entity_properties_files(self, buid):
+    def entity_properties_files(self, buid, output_as_str=True):
         buid = self.buid_normalizer(buid)        
-        files = self.entity_files(buid, self.property_file_glob)
+        files = self.entity_files(buid, self.property_file_glob, output_as_str=output_as_str)
         return list(files)
 
 
@@ -230,7 +293,7 @@ class BarelyDB(object):
         buid = self.buid_normalizer(buid)       
         old_properties = self.entity_properties.get(buid, {})
 
-        files = self.entity_properties_files(buid)
+        files = self.entity_properties_files(buid, output_as_str=False)
 
         properties = {}
         for fn in files:
@@ -260,36 +323,109 @@ class BarelyDB(object):
         for buid in buids:
             self.load_entity_properties(buid)           
 
-    
-    def get_entity_properties(self, buid):
+    def get_entity_properties(self, buid, reload = False):
         buid = self.buid_normalizer(buid)       
 
-        if buid not in self.entity_properties:
+        if (buid not in self.entity_properties) or reload:
             self.load_entity_properties(buid)
 
         return self.entity_properties[buid]
 
+    def get_entity_tree(self, buid):
+        buid = self.buid_normalizer(buid)       
+        tree = objectpath.Tree(self.get_entity_properties(buid))
+        return tree
 
-    # def query_property(self, buid, prop_name, property_file = '', source = ''):
-    #     buid = self.buid_normalizer(buid)       
-    #     tree = objectpath.Tree(self.get_entity_properties(buid))
+    def add_preferred_file(self, prop_file):
+        self.preferred_property_files = list(set(self.preferred_property_files + [prop_file]))
 
-    #     prop_name = str(prop_name)
-    #     property_file = str(property_file)
-    #     source = str(source)
+    def clear_preferred_files(self):
+        self.preferred_property_files = []
 
-    #     # query =  f'$..*[("{property_file}" in @.property_file)]'
-    #     query =  f'$..*[("{prop_name}" in @)]'
-    #     query += f'.({prop_name}, property_file, source)'
+    def query_property(self, buid, prop, property_file = '', source = '', warn_empty = True):
+        buid = self.buid_normalizer(buid)       
+
+        prop = str(prop)
+        property_file = str(property_file)
+        source = str(source)
+
+        tree = self.get_entity_tree(buid)
+
+        # query =  f'$..*[("{property_file}" in @.property_file)]'
+
+        # query property together with file and source information
+        selector = f'({prop} in @)'
+        selector += f'and ("{property_file}" in @.property_file)'
+        selector += f'and ("{source}" in @.source)'
+        selector = f'[{selector}]'
+
+        query =  f'$..*{selector}'
+        query += f'.({prop}, property_file, source)'
+        result = list(tree.execute(query))
+
+        def make_item(res):
+            return SourcedItem(name=prop, 
+                        value=res[prop], 
+                        property_file=res['property_file'], 
+                        source=res['source'])
+
+        if len(result) == 0:
+            if warn_empty:
+                self.logger.warn(f'No value for {prop} in entity {buid}!')
+            return None
+        elif len(result) == 1:
+            return make_item(result[0])
+        elif len(result) > 1:
+            # try to remove ambiguity by self.preferred_property_files
+            
+            selector = [f'("{p}" in @.property_file)' for p in self.preferred_property_files]
+            selector = ' or '.join(selector)
+            selector = f'[{selector}]' if selector else ''
+            # self.logger.warn(str(selector))
+
+            tree = objectpath.Tree(result)
+            result_amb = list(tree.execute(f'$..*{selector}'))            
+
+            if len(result_amb) == 0:
+                self.logger.warn(f'Multiple sources for {prop} in entity {buid}! Filtering with preferred files did not yield a result. Please, refine this query adding a property file to preferred_property_files! Found sources follow below:')
+                for res in result:
+                    self.logger.warn(json.dumps(res, indent=4))
+                return None
+            elif (len(result_amb) == 1):
+                return make_item(result_amb[0])
+            elif (len(result_amb) > 1):
+                self.logger.warn(f'Multiple sources for {prop} in entity {buid}, even after filtering with preferred_property_files! Please, remove entries from preferred_property_files to lift this ambiguity! Found sources follow below:')
+                for res in result_amb:
+                    self.logger.warn(json.dumps(res, indent=4))
+                return None
         
-    #     result = list(tree.execute(query)
+        # shouldnt arrive here
+        return None
 
         # list(tree.execute("$..*[tsc in @].property_file"))
 
-        # query = f'$..*[("{str(property_file)}" in @.property_file)].{str(prop_name)}'
+        # query = f'$..*[("{str(property_file)}" in @.property_file)].{str(prop)}'
         # result = list(tree.execute(query)
 
 
+    def query_properties(self, buid, props):
+        ''' Queries a list of properties. 
+
+        Parameters: 
+        props is a list of strings or dictionaries or mixed. 
+        Strings are interpreted as the prop parameter to query_properties.
+        Dictionaries as keyword parameters to query_properties.
+        '''
+        buid = self.buid_normalizer(buid)       
+
+        result = []
+        for p in props:
+            if type(p) == dict:
+                result.append(self.query_property(buid, **p)) 
+            else:
+                result.append(self.query_property(buid, str(p))) 
+
+        return result
 
 
 
